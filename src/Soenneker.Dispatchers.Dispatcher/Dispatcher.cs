bf65e3b66f71bf35b15fd46ddb1f845dbd852091
soenneker <jake@soenneker.com>
2026-09-03 22:01:16 -0500

    Add interface inheritdoc references

--- src/Soenneker.Dispatchers.Dispatcher/Dispatcher.cs
+++ src/Soenneker.Dispatchers.Dispatcher/Dispatcher.cs
@@ -3,6 +3,7 @@ using Soenneker.Dispatchers.Dispatcher.Abstract;
 
 namespace Soenneker.Dispatchers.Dispatcher;
 
+/// <inheritdoc cref="IDispatcher" />
 public abstract class Dispatcher : IDispatcher
 {
     protected ILogger<Dispatcher> Logger { get; }
